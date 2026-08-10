341fba7e9916a540a7692e814f354008cd1c87c4
sebastiancarlos8 <davsebca@gmail.com>
2026-08-09 20:26:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title("Especialización Python for Analytics")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Sebastian Carlos")
+
+valor_inicial = st.number_input("Ingrese el valor inicial")
+valor_final = st.number_input("Ingrese el valor final")
