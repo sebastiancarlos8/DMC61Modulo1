9c4b32726d8ff0f9edfd6a89cd1d843a930d543b
sebastiancarlos8 <davsebca@gmail.com>
2026-08-09 21:19:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,9 @@ st.write(lista_numeros)
 
 modulos = st.sidebar.selectbox("Seleccione un módulo",["Módulo Listas","Módulo Arreglos","Módulo Funciones"])
 
+if modulos == "Módulo Listas":
+  st.write("Bienvenido al módulo de Listas")
+elif modulos == "Módulo Arreglos":
+  st.write("Bienvenido al módulo de Arreglos")
+else:
+  st.write("Bienvenido al módulo de Funciones")
