5a83fac83934a7d46b1cdedd897b78d69b9bd233
sebastiancarlos8 <davsebca@gmail.com>
2026-08-09 21:20:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,17 @@ st.title("Especialización Python for Analytics")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Sebastian Carlos")
 
-valor_inicial = st.number_input("Ingrese el valor inicial")
-valor_final = st.number_input("Ingrese el valor final")
-
-lista_numeros = list(range(int(valor_inicial),int(valor_final)))
-st.write(lista_numeros)
-
 modulos = st.sidebar.selectbox("Seleccione un módulo",["Módulo Listas","Módulo Arreglos","Módulo Funciones"])
 
 if modulos == "Módulo Listas":
   st.write("Bienvenido al módulo de Listas")
+
+  valor_inicial = st.number_input("Ingrese el valor inicial")
+  valor_final = st.number_input("Ingrese el valor final")
+  
+  lista_numeros = list(range(int(valor_inicial),int(valor_final)))
+  st.write(lista_numeros)
+
 elif modulos == "Módulo Arreglos":
   st.write("Bienvenido al módulo de Arreglos")
 else:
