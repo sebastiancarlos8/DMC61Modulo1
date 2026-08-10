7b51d3976d639f74de0dc4163332ee6ccc092aec
sebastiancarlos8 <davsebca@gmail.com>
2026-08-09 20:29:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ st.write("Elaborado por: Sebastian Carlos")
 valor_inicial = st.number_input("Ingrese el valor inicial")
 valor_final = st.number_input("Ingrese el valor final")
 
-lista_numeros = list(range(valor_inicial,valor_final))
+lista_numeros = list(range(int(valor_inicial),int(valor_final)))
 st.write(lista_numeros)
