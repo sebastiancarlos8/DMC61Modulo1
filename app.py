e955fb153c55ae9bb4a2070d0556c50cd354d196
sebastiancarlos8 <davsebca@gmail.com>
2026-08-15 14:01:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ st.title("Especialización Python for Analytics")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Sebastian Carlos")
 
-st.image("Python_logo.png", width = 50)
+st.image("Python_logo.png", width = 300)
+st.sidebar.image("DMC.png", width = 100)
 
 modulos = st.sidebar.selectbox("Seleccione un módulo",["Módulo Listas","Módulo Arreglos","Módulo Funciones"])
 
